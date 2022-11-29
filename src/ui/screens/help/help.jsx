1f754c9b6ae6b0ef6737c2aaf040e07c3bb9c082
Carlos Eduardo Kasper <52386569+CarlosKasper@users.noreply.github.com>
2022-11-28 23:48:33 -0300

    batata

---
+++ src/ui/screens/help/help.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from '../../components';
+import './index.scss'
+
+export function HelpScreen() {
+
+
+	return (
+    <div className="home-container">
+      <Header />
+      <div className="home-container__posts">
+        <label>texto aqui</label>
+      </div>
+    </div>
+	);
+}
